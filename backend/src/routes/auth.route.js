a5fc339c23d7f4757f56fc4dc97ad48720829646
Bharat-dixit-0512 <dixitbt251@gmail.com>
2026-03-10 01:15:42 +0530

    initial Commit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const router=express.Router();
+
+
+router.get('/signup',(req,res)=>{
+    res.send("SignUp EndPoint")
+})
+
+router.get('/login',(req,res)=>{
+    res.send("Login EndPoint")
+})
+
+router.get('/logout',(req,res)=>{
+    res.send("Logout EndPoint")
+})
+
+
+export default router;
