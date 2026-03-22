b995089e9df5d24429d77497e027ac0dc0742d3c
Bharat-dixit-0512 <dixitbt251@gmail.com>
2026-03-22 12:01:14 +0530

    message route

--- backend/src/lib/arcjet.js
+++ backend/src/lib/arcjet.js
@@ -1,16 +1,18 @@
 import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
 import { ENV } from "./env.js";
 
+const arcjetMode = ENV.NODE_ENV === "production" ? "LIVE" : "DRY_RUN";
+
 const aj = arcjet({
   // Get your site key from https://app.arcjet.com and set it as an environment
   // variable rather than hard coding.
   key: ENV.ARCJET_KEY,
   rules: [
     // Shield protects your app from common attacks e.g. SQL injection
-    shield({ mode: "LIVE" }),
+    shield({ mode: arcjetMode }),
     // Create a bot detection rule
     detectBot({
-      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+      mode: arcjetMode,
       // Block all bots except the following
       allow: [
         "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
@@ -22,7 +24,7 @@ const aj = arcjet({
     }),
     // Create a token bucket rate limit. Other algorithms are supported.
     slidingWindow({
-        mode:"LIVE",
+        mode: arcjetMode,
         max:100,
         interval:60
     })
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,23 @@
 import express from "express";
+import {
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+  getChatPartners,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
-const router=express.Router();
 
-router.get('/send',(req,res)=>{
-    res.send("Send Message endPoint");
-});
+const router = express.Router();
+router.use(arcjetProtection,protectRoute);
 
 
-export default router
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.get("/send/:id", sendMessage);
+
+
+
+export default router;
